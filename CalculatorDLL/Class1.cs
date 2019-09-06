8250f37bf7d5585ef8ae11dc532dab6b8dbddeae
Jez Horton <JHorton@spartaglobal.com>
2019-09-06 09:11:36 +0100

    Initial Commit

---
+++ CalculatorDLL/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calc
+{
+    public class Calculator
+    {
+        public double Divide(double one, double two)
+        {
+            return one / two;
+        }
+        public double Add(double one, double two)
+        {
+            return one + two;
+        }
+        public double Multiply(double one, double two)
+        {
+            return one * two;
+        }
+        public double Subtract(double one, double two)
+        {
+            return one - two;
+        }
+    }
+}
---
+++ TestDB/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TestDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string yes = String.Format("This is true {0}", "cake");
+            using (SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=testDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")){
+                con.Open();
+                Console.WriteLine(con.State);
+                SqlCommand insertCommand = new SqlCommand("INSERT INTO pls (name, id) VALUES ('S', '2')", con);
+                SqlCommand selectCommand = new SqlCommand("SELECT * FROM pls", con);
+                SqlCommand updateString = new SqlCommand("UPDATE pls SET name = 'John' WHERE id = '2'");
+                SqlCommand deleteString = new SqlCommand("DELETE FROM ");
+                insertCommand.ExecuteReader();
+                //selectCommand.Read;
+                con.Close();
+            }
+
+            Test test1 = new Test();
+            test1.Type = "Dancing queen";
+
+        }
+    }
+    class Test
+    {
+        string name = "", type = "", genre = "";
+
+        public string Name { get => name; set => name = value; }
+        public string Type { get => type; set => type = value; }
+        public string Genre { get => genre; set => genre = value; }
+    }
+}
